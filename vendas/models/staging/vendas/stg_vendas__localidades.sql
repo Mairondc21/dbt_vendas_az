9aff3071252411aa58b1733cb26bd2b8cd054d28
Mairon.az <duartemairon@gmail.com>
2025-01-07 23:38:21 -0400

    update

--- vendas/models/staging/vendas/stg_vendas__localidades.sql
+++ vendas/models/staging/vendas/stg_vendas__localidades.sql
@@ -1,7 +1,7 @@
 WITH source AS (
     SELECT
         id_localidade AS pk_lo_id,
-        nome_cidade AS lo_nome,
+        nome_cidade AS lo_cidade,
         nome_estado AS lo_estado,
         nome_regiao AS lo_regiao
     FROM

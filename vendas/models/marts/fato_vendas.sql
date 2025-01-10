7eb9b740821e5f9c067f4eb0d17e54af8ee07b39
Mairon.az <duartemairon@gmail.com>
2025-01-10 09:10:26 -0400

    update

--- vendas/models/marts/fato_vendas.sql
+++ vendas/models/marts/fato_vendas.sql
@@ -7,15 +7,15 @@ WITH source AS (
         vd.iv_quantidade AS quantidade,
         vd.pr_preco AS preco_uni,
         vd.iv_preco_total AS preco_total,
-        vd.vd_dia_venda AS dia_venda,
         vd.vd_mes_venda AS mes_venda,
         vd.vd_ano_venda AS ano_venda,
         vd.vd_valor_total AS total_venda
     FROM
         {{ ref('int_vendas_consolidado') }} vd
-    INNER JOIN {{ref('dim_usuarios')}} us ON us.sk_dim_usuarios = vd.pk_us_id
+    INNER JOIN {{ref('snap_dim_usuarios')}} us ON us.sk_dim_usuarios = vd.pk_us_id
     INNER JOIN {{ref('dim_produtos')}} pr ON pr.sk_dim_produtos = vd.pk_pr_id
-    INNER JOIN {{ref('dim_lojas')}} lj ON lj.sk_dim_lojas = vd.pk_lj_id
+    INNER JOIN {{ref('snap_dim_lojas')}} lj ON lj.sk_dim_lojas = vd.pk_lj_id
+    WHERE us.dbt_valid_to IS NULL AND lj.dbt_valid_to IS NULL
     ORDER BY  pk_iv_id
 
 )
